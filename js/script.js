1f919935f02bfbb9c4c0b7470e1d6344e3e40c47
Liliia <LiliiaPav@gmail.com>
2024-04-03 11:34:19 -0700

    finished

--- js/script.js
+++ js/script.js
@@ -1,28 +1,73 @@
 // GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
-
+let fmEmpl = document.getElementById("addForm");
+let tblEmpl = document.getElementById("employees");
+let valCounter = document.getElementById("empCount");
 // SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
-
+let counter=0;
 
 // ADD EMPLOYEE
-form.addEventListener('submit', (e) => {
+fmEmpl.addEventListener('submit', (e) => {
+
     // PREVENT FORM SUBMISSION
+    e.preventDefault();
 
     // GET THE VALUES FROM THE TEXT BOXES
+    const $         = (id) => document.getElementById(id)
+    let valEmplId   = $("id").value;
+    let valEmplName = $("name").value;
+    let valEmplExt  = $("extension").value;
+    let valEmplEmail= $("email").value;
+    let valEmplDep  = $("department").value;
 
     // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+    let tr      = tblEmpl.insertRow();
 
     // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    let tdID    =tr.insertCell();
+    let tdName  =tr.insertCell();
+    let tdExt   =tr.insertCell();
+    let tdEmail =tr.insertCell();
+    let tdDep   =tr.insertCell();
+    let tdDel   =tr.insertCell();
 
     // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
+    let textID=document.createTextNode(valEmplId);
+    tdID.appendChild(textID);
+    let textName=document.createTextNode(valEmplName);
+    tdName.appendChild(textName);
+    let textExt=document.createTextNode(valEmplExt);
+    tdExt.appendChild(textExt);
+    let textEmail=document.createTextNode(valEmplEmail);
+    tdEmail.appendChild(textEmail);
+    let textDep=document.createTextNode(valEmplDep);
+    tdDep.appendChild(textDep);
 
     // CREATE THE DELETE BUTTON
+    let deleteBtn=document.createElement('button');
+    let text=document.createTextNode('X');
+    deleteBtn.appendChild(text);
+    deleteBtn.className='btn btn-sm bg-danger text-white text-center';
+    tdDel.appendChild(deleteBtn);
 
     // RESET THE FORM
+    fmEmpl.reset();
 
     // SET FOCUS BACK TO THE ID TEXT BOX
+    $("id").focus();
 
     // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
+    valCounter.value=counter+1;
+    valCounter.className="text-muted";
 
 })
 
 // DELETE EMPLOYEE
+tblEmpl.addEventListener('click', (e) =>{
+    console.log(e.target.parentElement.cellIndex)
+    if(e.target.parentElement.cellIndex==5){
+        if (confirm('Are you sure you want to delete this employee?')){
+            tblEmpl.deleteRow(e.target.parentElement.parentElement.rowIndex);
+            valCounter.value=counter-1;
+        }
+    }
+})
